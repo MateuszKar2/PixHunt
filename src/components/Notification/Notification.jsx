09b354e21599ed51c5304536269c81c405b6dd96
MateuszKar2 <system505@protonmail.com>
2023-09-25 23:15:13 +0200

    remove file

--- src/components/Notification/Notification.jsx
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-// import css from './Notification.module.css';
-
-export const Notification = ({message}) => <p>{message}</p>;
-
-Notification.protoTypes = {
-    message: PropTypes.string.isRequired
-}
