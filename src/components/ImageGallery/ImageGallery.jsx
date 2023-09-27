7f268199a9afc86c559232e11f9fc662726fd5d2
MateuszKar2 <system505@protonmail.com>
2023-09-27 09:22:18 +0200

    add file

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,43 +1,42 @@
-import React,{Component} from "react";
-import css from "./ImageGallery.module.css";
+import { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { nanoid } from 'nanoid';
-import {ImageGalleryItem} from './ImageGalleryItem/ImageGalleryItem';
-
+import css from './ImageGallery.module.css';
 
 export class ImageGallery extends Component {
-    onClickHandle = i => {
-        return e => {
-            e.preventDefault();
-            this.props.stateUpdate('modalIndex', i);
-        };
+  onClickHandle = i => {
+    return e => {
+      e.preventDefault();
+      this.props.stateUpdate('modalIndex', i);
     };
-    render() {
-        return(
-            <ul className={css.ImageGallery}>
-                {this.props.gallery.map((img, i) => {
-                    <ImageGalleryItem
-                        key={nanoid()}
-                        id={img.id}
-                        miniature={img.miniature}
-                        url={img.url}
-                        alt={img.tags}
-                        onClick={this.onClickHandle(i)}
-                    />
-                })}
-            </ul>
-        )
-    }
+  };
+  render() {
+    return (
+      <ul className={css.ImageGallery}>
+        {this.props.gallery.map((img, i) => (
+          <ImageGalleryItem
+            key={nanoid()}
+            id={img.id}
+            miniature={img.miniature}
+            url={img.url}
+            alt={img.tags}
+            onClick={this.onClickHandle(i)}
+          />
+        ))}
+      </ul>
+    );
+  }
 }
 
 ImageGallery.propTypes = {
-    gallery: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            url: PropTypes.string,
-            miniature: PropTypes.string,
-            alt: PropTypes.string,
-        })
-    ).isRequired,
-    stateUpdate: PropTypes.func.isRequired,
+  gallery: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      url: PropTypes.string,
+      miniature: PropTypes.string,
+      alt: PropTypes.string,
+    })
+  ).isRequired,
+  stateUpdate: PropTypes.func.isRequired,
 };
