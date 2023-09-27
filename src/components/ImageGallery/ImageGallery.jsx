904874c0563c203049e25dd01f03b396f88c0efc
MateuszKar2 <system505@protonmail.com>
2023-09-27 09:14:33 +0200

    add ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,15 +1,43 @@
-import React,{Component} from "react"
-import css from "./ImageGallery.module.css"
+import React,{Component} from "react";
+import css from "./ImageGallery.module.css";
+import { PropTypes } from 'prop-types';
+import { nanoid } from 'nanoid';
+import {ImageGalleryItem} from './ImageGalleryItem/ImageGalleryItem';
 
 
-class ImageGallery extends Component {
-
+export class ImageGallery extends Component {
+    onClickHandle = i => {
+        return e => {
+            e.preventDefault();
+            this.props.stateUpdate('modalIndex', i);
+        };
+    };
     render() {
         return(
-            <ul class={css.gallery}>
+            <ul className={css.ImageGallery}>
+                {this.props.gallery.map((img, i) => {
+                    <ImageGalleryItem
+                        key={nanoid()}
+                        id={img.id}
+                        miniature={img.miniature}
+                        url={img.url}
+                        alt={img.tags}
+                        onClick={this.onClickHandle(i)}
+                    />
+                })}
             </ul>
         )
     }
 }
 
-export default ImageGallery;
+ImageGallery.propTypes = {
+    gallery: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            url: PropTypes.string,
+            miniature: PropTypes.string,
+            alt: PropTypes.string,
+        })
+    ).isRequired,
+    stateUpdate: PropTypes.func.isRequired,
+};
