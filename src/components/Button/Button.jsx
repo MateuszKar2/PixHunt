1474c5c5c4cfa021ddb0b64af26c0ba9b0a54da1
MateuszKar2 <system505@protonmail.com>
2023-09-27 09:14:18 +0200

    add Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,2 +1,15 @@
-import React,{Component} from "react"
-import css from "./Button.module.css"
+import css from "./Button.module.css";
+import { PropTypes } from 'prop-types';
+
+export const Button = ({ onClick, txt }) => {
+    return (
+        <button className={css.button} type="button" onClick={onClick}>
+            {txt}
+        </button>
+    );
+};
+
+Button.propTypes = {
+    txt: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
